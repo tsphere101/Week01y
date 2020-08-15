d16255dd1ba9cced950365ddc2a69f7f0ffb3e2f
Topfee <Topfee@TOPFEELAPTOP>
2020-08-15 13:26:05 +0700

    2

--- Week01Y/Source.cpp
+++ Week01Y/Source.cpp
@@ -2,23 +2,32 @@
 //Trapezoid
 #include <stdio.h>
 
-typedef struct{
-	double sideA;
-	double sideB;
-	double height;
+typedef struct
+{
+    double sideA;
+    double sideB;
+    double height;
 } trapezoid;
+
+double trapezoidArea(double h, double a, double b)
+{
+    double result = 0.5 * h * (a + b);
+    return result;
+}
 
 int main()
 {
-	trapezoid myTrapezoid;
-	printf("To find the area of the Trapezoid\n Enter the height followed by both of the sides of the trapezoid\n(separated by space) :");
-	scanf("%lf %lf %lf",&myTrapezoid.height, &myTrapezoid.sideA, &myTrapezoid.sideB);
+    trapezoid myTrapezoid;
+    printf("To find the area of the Trapezoid\n Enter the height followed by both of the sides of the trapezoid\n(separated by space) :");
+    scanf("%lf %lf %lf", &myTrapezoid.height, &myTrapezoid.sideA, &myTrapezoid.sideB);
 
-	if (myTrapezoid.height <= 0 || myTrapezoid.sideA <=0 || myTrapezoid.sideB <= 0) goto err;
+    if (myTrapezoid.height <= 0 || myTrapezoid.sideA <= 0 || myTrapezoid.sideB <= 0)
+        goto err;
 
-	printf("\nThe area of the Trapezoid is \n 0.5 x %.2f x( %.2f + %.2f) \n = %.2f \n", myTrapezoid.height, myTrapezoid.sideA, myTrapezoid.sideB,0.5*myTrapezoid.height*(myTrapezoid.sideA+myTrapezoid.sideB));
+    printf("\nThe area of the Trapezoid is \n 0.5 x %.2f x( %.2f + %.2f) \n = %.2f \n", myTrapezoid.height, myTrapezoid.sideA, myTrapezoid.sideB, trapezoidArea(myTrapezoid.height, myTrapezoid.sideA, myTrapezoid.sideB));
 
-	return 0;
+    return 0;
 
-err: printf("Error!");
+err:
+    printf("Error!");
 }
